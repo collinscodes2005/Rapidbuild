4b362699bbdf693684d34b528a1053d8144f411b
Ikotun Collins <danlogan2003@gmail.com>
2023-04-14 14:50:48 +0100

    Access and refresh tokens

--- app.js
+++ app.js
@@ -14,22 +14,38 @@ const app = Vue.createApp({
   methods: {
     //function to set the employee info(when the index is passed in)
     async getUser(index) {
-      if (index < this.employee_data.length) {
+      if (this.loggedIn && index < this.employee_data.length) {
         this.first_name = this.employee_data[index].first_name;
         this.current_index = index;
         console.log('hey')
         console.log(this.first_name)
-
+      } else if (this.loggedIn) {
+        const access_token = localStorage.getItem('access_token');
+        fetch("http://127.0.0.1:8000/employees/", {
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          Authorization: `Bearer ${access_token}`
+        })
+        .then(response => response.json())
+        .then(data => {
+          this.employee_data = data;
+          app.mount("#app");
+          this.getUser(0);
+        })
+        .catch(error => console.error(error));
       }
     },
-    //function to get the index of the next employee 
+    
+    //NEXT EMPLOYEE 
     nextEmployee() {
       const next_index = this.current_index + 1;
-      if (next_index < this.employee_data.length) {
-        this.getUser(next_index);
+      if (next_index < this.employee_data.length){
+        this.getUser(next_index)
       }
-      console.log("it worked")
     },
+
     //login function 
     async login(){
       const res = await fetch("http://127.0.0.1:8000/login/", {
@@ -41,47 +57,51 @@ const app = Vue.createApp({
         body: JSON.stringify({
           user_name: this.username,
           password: this.password,
-
         }),
       });
-
+    
       const data = await res.json();
       // Check if login was successful
       if (data.success) {
         this.first_page = false;
         this.loggedIn = true;
         this.getUser(0);
-
+    
         // Store tokens in local storage
         localStorage.setItem('access_token', data.access);
         localStorage.setItem('refresh_token', data.refresh);
-
+    
         // Retrieve tokens from local storage
         const access_token = localStorage.getItem('access_token');
         const refresh_token = localStorage.getItem('refresh_token');
-
+    
+        console.log(access_token)
+        console.log(refresh_token)
+    
+        // Set authorization header for future API requests
+        // fetch.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+    
+        if (access_token) {
+          fetch("http://127.0.0.1:8000/employees/", {
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${access_token}`
+            }
+          })
+          .then(response => response.json())
+          .then(data => {
+            this.employee_data = data;
+            this.getUser(0);
+          })
+          .catch(error => console.error(error));
+        }
+    
       } else {
         // Display error message
         alert('Invalid username or password.');
       }
     },
+    
   },
-
-
-  mounted() {
-      fetch("http://127.0.0.1:8000/employees/", {
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        this.employee_data = data;
-        this.getUser(0);
-      })
-      .catch(error => console.error(error));
-
-    },
-});
-app.mount("#app");
+}).mount("#app");
